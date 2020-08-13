8d46716a68aa7870f063dffbd1ca35966f38c21a
Mikael <68263461+NordicFlame@users.noreply.github.com>
2020-08-14 08:53:01 +1000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBL-d1RQZI_DL0xjFQxvkHVoLx6b4BAQsw",
-    authDomain: "mits-bict-helpsite.firebaseapp.com",
-    databaseURL: "https://mits-bict-helpsite.firebaseio.com",
-    projectId: "mits-bict-helpsite",
-    storageBucket: "mits-bict-helpsite.appspot.com",
-    messagingSenderId: "566567351630",
-    appId: "1:566567351630:web:b00ff8bd2ed95636"
+    apiKey: "", //removed for security as this is sitting in a public space
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 
 };
